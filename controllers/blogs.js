e19b7ae20c63ae4142308c8f621ca60bf8ce2a05
Juha Hyvärinen <n7hyju00@students.oamk.fi>
2023-02-22 15:51:22 +0200

    Added exercise 13.5 - 13.6

---
+++ controllers/blogs.js
@@ -0,0 +1,55 @@
+const router = require('express').Router()
+
+const { Blog } = require('../models')
+
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  console.log(JSON.stringify(blogs, null, 2))
+  res.json(blogs)
+})
+  
+router.get('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    console.log(req.blog.toJSON())
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    console.log(req.body)
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy()
+    res.status(204).end()
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json({ likes: req.blog.likes })
+    //res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+module.exports = router
